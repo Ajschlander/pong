b6dfe0b07f0a7480fea011f2df2f7aceeba5eb85
Ajschlander <Aschlander21@gmail.com>
2020-01-24 13:42:04 -0600

    added usereducer for ball

--- src/App.js
+++ src/App.js
@@ -1,12 +1,92 @@
-import React from 'react';
+import React, {useEffect, useReducer} from 'react';
 import './App.css';
 import Paddle from './components/Paddle';
+import Ball from './components/Ball';
+
+const initialState = {
+  paddle: {
+    y: 0
+  },
+  ball: {
+    x: 0,
+    y: 0,
+    dx: 5,
+    dy: 5
+  }
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "MOVE_PADDLE":
+      return { ...state, paddle: action.payload };
+    case "MOVE_BALL":
+      return { ...state, ball: action.payload };
+    default:
+      throw new Error();
+  }
+}
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  function handleMouse(e) {
+    let boundedY;
+    const offset = (window.innerWidth - 600) / 2;
+    if (e.y - offset < 0) {
+      boundedY = 0;
+    } else if (e.y - offset > 450) {
+      boundedY = 450;
+    } else {
+      boundedY = e.y - offset;
+    }
+    dispatch({
+      type: "MOVE_PADDLE",
+      payload: {
+        y: boundedY
+      }
+    });
+  }
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouse);
+  }, []);
+
+  useEffect(() => {
+    const handle = setTimeout(() => {
+      let x = state.ball.x;
+      let y = state.ball.y;
+      let dx = state.ball.dx;
+      let dy = state.ball.dy;
+
+      let paddleY = state.paddle.y;
+
+      if (dx + dx > 1200 - 20 || dx + dx < 0) {
+        dx = -dx;
+      }
+      if (y + dy > 800 - 20 || y + dy < 0) {
+        dy = -dy;
+      }
+
+      if((x > 340 || x < 340) && paddleY < y + dy && paddleY + 100 > y + dy){
+        dx = -dx;
+      }
+
+      dispatch({
+        type: "MOVE_BALL",
+        payload: {
+          dx,
+          dy,
+          x: state.ball.x,
+          y: state.ball.y
+        }
+      });
+    }, 50);
+    return () => clearTimeout(handle);
+  }, [state.ball]);
   return (
     <div className="container">
       <Paddle />
       <Paddle isPlayerTwo />
+      <Ball />
     </div>
   );
 }
--- src/components/Paddle.js
+++ src/components/Paddle.js
@@ -1,20 +1,31 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from "react";
 import "./Paddle.css";
 
-export default function Paddle({isPlayerTwo}){
-    let [paddleY, setPaddleY] = useState(0);
-
-    let handleMouse = e => {
-        setPaddleY(e.y);
+export default function Paddle({isPlayerTwo}) {
+  const [paddleY, setPaddleY] = useState(0);
+  function handleMouse(e) {
+    let boundedY;
+    const offset = (window.innerHeight - 600) / 2;
+    if (e.y - offset < 0) {
+      boundedY = 0;
+    } else if (e.y - offset > 450) {
+      boundedY = 450;
+    } else {
+      boundedY = e.y - offset;
     }
-
-    useEffect(() => {
-        window.addEventListener("mousemove", handleMouse)
-    }, [])
-
-    return (
-        <div className={isPlayerTwo ? "paddle player2" : "paddle"} style={{top: `${paddleY}px`}}>
-
-        </div>
-    )
+    setPaddleY(boundedY);
+  }
+  useEffect(() => {
+    window.addEventListener("mousemove", handleMouse);
+  }, []);
+  return (
+    <div
+    className={isPlayerTwo ? "paddle player2" : "paddle"}
+      isPlayerTwo
+      style={{
+        top: `${paddleY}px`
+        // transform: `translateX(calc(-50% + ${paddleX}px))`
+      }}
+    />
+  );
 }
